801245100190ecfd6b7ff68c253a0d816f47a415
Sarra <sarrasoussia2001@gmail.com>
2024-08-25 22:14:03 +0100

    done

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,11 +1,11 @@
-const cart = () => {
-    return (
-        <main className="pt-24 sm:pt-32 p-6 bg-white min-h-screen"> 
-            <h1 className="text-xl font-semibold text-700"> This is your cart:</h1>      
-                
-        </main>
-    )
+const Cart = () => {
 
-}
+  return (
+    <main className="pt-24 sm:pt-32 p-6 bg-white min-h-screen">
 
-export default cart;
+
+    </main>
+  );
+};
+
+export default Cart;
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <main className="pt-16 sm:pt-32 p-6 bg-white min-h-screen flex flex-col items-center"> 
-      <div className="w-full flex justify-center mb-6">
+      <div className="w-full flex justify-center mb-6 pt-6">
       <Link href="/map">
         <Button className="text-lg font-bold">View available offers around you</Button>
       </Link>
