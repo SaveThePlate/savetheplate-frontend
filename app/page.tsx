a8ab5baeead84a067ca92b2ed5227e637c79e2ee
Sarra <sarrasoussia2001@gmail.com>
2024-08-25 21:58:59 +0100

    fixed the main page: button to view the map

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import Offers from '@/components/Offers';
 import { Button } from '@/components/ui/button';
-
+import Link from 'next/link';
 
 const Home = () => {
-
   return (
-  <main className="pt-24 sm:pt-32 p-6 bg-white min-h-screen"> 
+    <main className="pt-16 sm:pt-32 p-6 bg-white min-h-screen flex flex-col items-center"> 
+      <div className="w-full flex justify-center mb-6">
+      <Link href="/map">
+        <Button className="text-lg font-bold">View available offers around you</Button>
+      </Link>
+      </div>
 
-    <div className="">
-      <Button className="">View available offers around you</Button>
-    </div>
-
-    <Offers/>
-  </main>
+      <Offers />
+    </main>
   );
-
 };
 
 export default Home;
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div className="flex justify-around items-center bg-white shadow-md fixed bottom-0 w-full h-16 border-t border-gray-200">
+    <footer className="flex justify-around items-center bg-white shadow-md fixed bottom-0 w-full h-16 border-t border-gray-200">
+      <Link href="/" className="h-full flex items-center">
+        <button className="hover:bg-gray-200 text-gray-800 font-bold h-full px-4 rounded flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+            <path d="M219.31,108.68l-80-80a16,16,0,0,0-22.62,0l-80,80A15.87,15.87,0,0,0,32,120v96a8,8,0,0,0,8,8h64a8,8,0,0,0,8-8V160h32v56a8,8,0,0,0,8,8h64a8,8,0,0,0,8-8V120A15.87,15.87,0,0,0,219.31,108.68ZM208,208H160V152a8,8,0,0,0-8-8H104a8,8,0,0,0-8,8v56H48V120l80-80,80,80Z"></path>
+          </svg>
+        </button>
+      </Link>
 
-    <Link href="/">
-    <button className=" hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M219.31,108.68l-80-80a16,16,0,0,0-22.62,0l-80,80A15.87,15.87,0,0,0,32,120v96a8,8,0,0,0,8,8h64a8,8,0,0,0,8-8V160h32v56a8,8,0,0,0,8,8h64a8,8,0,0,0,8-8V120A15.87,15.87,0,0,0,219.31,108.68ZM208,208H160V152a8,8,0,0,0-8-8H104a8,8,0,0,0-8,8v56H48V120l80-80,80,80Z"></path></svg>
-    </button>
-    </Link> 
+      <Link href="/cart" className="h-full flex items-center">
+        <button className="hover:bg-gray-200 text-gray-800 font-bold h-full px-4 rounded flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+            <path d="M230.14,58.87A8,8,0,0,0,224,56H62.68L56.6,22.57A8,8,0,0,0,48.73,16H24a8,8,0,0,0,0,16h18L67.56,172.29a24,24,0,0,0,5.33,11.27,28,28,0,1,0,44.4,8.44h45.42A27.75,27.75,0,0,0,160,204a28,28,0,1,0,28-28H91.17a8,8,0,0,1-7.87-6.57L80.13,152h116a24,24,0,0,0,23.61-19.71l12.16-66.86A8,8,0,0,0,230.14,58.87ZM104,204a12,12,0,1,1-12-12A12,12,0,0,1,104,204Zm96,0a12,12,0,1,1-12-12A12,12,0,0,1,200,204Zm4-74.57A8,8,0,0,1,196.1,136H77.22L65.59,72H214.41Z"></path>
+          </svg>
+        </button>
+      </Link>
 
+      <Link href="/profile" className="h-full flex items-center">
+        <button className="hover:bg-gray-200 text-gray-800 font-bold h-full px-4 rounded flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+            <path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path>
+          </svg>
+        </button>
+      </Link>
+    </footer>
+  );
+};
 
-    <Link href="/cart">
-    <button className=" hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M230.14,58.87A8,8,0,0,0,224,56H62.68L56.6,22.57A8,8,0,0,0,48.73,16H24a8,8,0,0,0,0,16h18L67.56,172.29a24,24,0,0,0,5.33,11.27,28,28,0,1,0,44.4,8.44h45.42A27.75,27.75,0,0,0,160,204a28,28,0,1,0,28-28H91.17a8,8,0,0,1-7.87-6.57L80.13,152h116a24,24,0,0,0,23.61-19.71l12.16-66.86A8,8,0,0,0,230.14,58.87ZM104,204a12,12,0,1,1-12-12A12,12,0,0,1,104,204Zm96,0a12,12,0,1,1-12-12A12,12,0,0,1,200,204Zm4-74.57A8,8,0,0,1,196.1,136H77.22L65.59,72H214.41Z"></path></svg>    </button>
-    </Link>
-    
-
-    <Link href="/profile">
-    <button className=" hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path></svg>    </button>
-    </Link>
-  
-
-
-  </div>
-
-  )
-
-}
-
-export default Footer
+export default Footer;
